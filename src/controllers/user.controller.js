99844296d369e8a67c9efea593f2fa882540530a
otsur <p.otsur@iitg.ac.in>
2026-05-20 17:49:39 +0530

    added watch history endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -400,6 +400,59 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
 
 })
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+     const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner" // gives an Object instead of an array
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+     ])
+
+     return res.status(200)
+               .json(new ApiResponse(200,
+                                     user[0].watchHistory, // from aggregation pipelines
+                                     "Watch history fetched successfully"
+                ))
+})
+
 export {
     registerUser,
     loginUser,
@@ -410,5 +463,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 }
